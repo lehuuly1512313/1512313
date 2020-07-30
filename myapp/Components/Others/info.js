5081e5af0d0bffde451b98bbb7e56c256f9217c8
lehuuly1512313 <lehuuly1512313@gmail.com>
2020-07-30 17:02:45 +0700

    Delete info.js

--- myapp/Components/Others/info.js
+++
@@ -1,7 +0,0 @@
-export default urls={
-"tgbNymZ7vqY": require('./../../DownloadFilePro/tgbNymZ7vqY.mp4'),
-"oFU6pjLJs2w": require('./../../DownloadFilePro/oFU6pjLJs2w.mp4'),
-"7eFingWiGBc": require('./../../DownloadFilePro/7eFingWiGBc.mp4'),
-"tgbNymZ7vqY": require('./../../DownloadFilePro/tgbNymZ7vqY.mp4'),
-"Sq_Qt8PWf_Y": require('./../../DownloadFilePro/Sq_Qt8PWf_Y.mp4'),
-}
